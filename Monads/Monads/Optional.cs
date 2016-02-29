34ffa8a0052a2072497aa1af3e9803b1311cf787
Oleksiy Syrotkin <oleksiy.syrotkin@elca.ch>
2016-02-29 14:00:22 +0100

    added sln

---
+++ Monads/Monads/Optional.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Monads {
+    public class Optional<T> where T : class {
+
+        public static Optional<T> Empty = new Optional<T>(null);
+
+        private readonly T m_value;
+
+        public Optional(T value) {
+            m_value = value;
+        }
+
+        public T Value {
+            get {
+                return m_value;
+            }
+        }
+        
+        /// <summary>
+        /// "FlatMap". Defines Monad's policy for function composition
+        /// </summary>
+        public Optional<TB> Bind<TB>(Func<T, Optional<TB>> function) where TB : class {
+            if (m_value == null) {
+                return Optional<TB>.Empty;
+            }
+            return function(m_value);
+        }
+
+        /// <summary>
+        /// Defines Monad's policy for function application
+        /// </summary>
+        public Optional<TB> Map<TB>(Func<T, TB> function) where TB : class {
+            return Bind(x => new Optional<TB>(function(x)));
+        }
+
+    }
+
+    public class Person {
+        public Optional<Car> Car {
+            get;
+            set;
+        }
+    }
+
+    public class Car {
+
+        public Optional<Insurance> Insurance {
+            get;
+            set;
+        }
+    }
+
+    public class Insurance {
+        public string Name {
+            get;
+            set;
+        }
+    }
+
+}
---
+++ Monads/Monads/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Monads {
+    class Program {
+        static void Main() {
+            var persons = GetPersons();
+            var names = persons.Select(GetCarInsuranceName);
+            foreach (var name in names) {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Done. Press any key...");
+            Console.ReadKey();
+        }
+        
+        private static string GetCarInsuranceName(Optional<Person> optionalPerson) {
+            return optionalPerson
+                .Bind(p => p.Car)
+                .Bind(c => c.Insurance)
+                .Map(i => i.Name)       // OR: .Bind(i => new Optional<string>(i.Name))
+                .Value ?? "Unknown";
+        }
+
+        // Create test data
+        private static IEnumerable<Optional<Person>> GetPersons() {
+            return new List<Optional<Person>> {
+
+                new Optional<Person>(new Person {
+                    Car = new Optional<Car>(new Car {
+                        Insurance = new Optional<Insurance>(new Insurance {
+                            Name = "Axa"
+                        })
+                    })
+                }),
+
+                new Optional<Person>(new Person {
+                    Car = new Optional<Car>(new Car {
+                        Insurance = new Optional<Insurance>(new Insurance())
+                    })
+                }),
+
+                new Optional<Person>(new Person {
+                    Car = new Optional<Car>(new Car {
+                        Insurance = new Optional<Insurance>(null)
+                    })
+                }),
+
+                new Optional<Person>(new Person {
+                    Car = new Optional<Car>(null)
+                }),
+
+                new Optional<Person>(null),
+            };
+        }
+    }
+}
